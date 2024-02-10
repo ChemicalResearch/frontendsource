2959ac048c4cfa0107cb47543ddc0c4fca8933e0
Goutam Das <goutamdas676@gmail.com>
2024-02-11 00:09:30 +0530

    Design

--- src/pages/SampleCollection.tsx
+++ src/pages/SampleCollection.tsx
@@ -7,6 +7,27 @@ function SampleCollection() {
             <header className="px-5 py-4 border-b border-gray-100">
               <h2 className="font-semibold text-gray-800">Job Number: 123456789</h2>
               <h2 className="font-semibold text-gray-800">Assigned On: 12-December-2023 12:00:00</h2>
+              <div className="flex items-center gap-4 mb-2">
+                <div>
+                  <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Job List</label>
+                </div>
+                <div>
+                  <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option>United States</option>
+                    <option>Canada</option>
+                    <option>France</option>
+                    <option>Germany</option>
+                  </select>
+                </div>
+              </div>
+              <div className="flex items-center gap-4 mb-2">
+                <div>
+                  <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Vehicle Number</label>
+                </div>
+                <div>
+                <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="" />
+                </div>
+              </div>
               <div className="flex flex-col gap-2 mt-4 items-start">
                 <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">Generate QR</button>
               </div>
