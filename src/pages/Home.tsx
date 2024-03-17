dec3e522e7f882994e0bcf1f1570195d90e812f6
Goutam Das <goutamdas676@gmail.com>
2024-03-17 23:04:34 +0530

    chart

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,29 @@
+import {Chart, ArcElement} from 'chart.js'
+
+import { Pie } from "react-chartjs-2";
+
+Chart.register(ArcElement);
+
+const labels = ["January", "February", "March", "April", "May", "June"];
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: "My First dataset",
+      backgroundColor: "rgb(255, 99, 132)",
+      borderColor: "rgb(0,0,255)",
+      data: [0, 10, 5, 2, 20, 30, 45],
+    },
+  ],
+};
+
 function Home() {
-    return (
+  return (
+    <div>
       <h1>Home</h1>
-    )
-  }
-  
-  export default Home;
-  
+      <Pie data={data} />
+    </div>
+  )
+}
+
+export default Home;
