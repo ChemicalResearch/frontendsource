630792fcd7022ad00597d283f089f28d2e8db736
Goutam Das <goutamdas676@gmail.com>
2024-03-20 19:59:57 +0530

    fixed

--- src/components/RequiredAuth.tsx
+++ src/components/RequiredAuth.tsx
@@ -3,7 +3,8 @@ import { useAuth } from "../context/auth"
 import { Navigate } from "react-router-dom";
 
 const RequiredAuth: FC<{ children: ReactNode }> = ({ children }) => {
-    const { isAuth } = useAuth();
+    const { isAuth, isLoading } = useAuth();
+    if(isLoading) return <p>Loading...</p>
     if (isAuth) {
         return children;
     }
