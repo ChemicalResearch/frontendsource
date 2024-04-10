245efec03b11e4202e1abd2bbc26072552a034c0
Goutam Das <goutamdas676@gmail.com>
2024-04-10 23:36:41 +0530

    remove comodity group

--- src/pages/⁠⁠LabHeadAssignment.tsx
+++ src/pages/⁠⁠LabHeadAssignment.tsx
@@ -38,9 +38,6 @@ const LabHeadAssignment = () => {
                       <tr>
                         <th className="p-2 whitespace-nowrap"></th>
                         <th className="p-2 whitespace-nowrap">
-                          <div className="font-semibold text-left">Commodity Group</div>
-                        </th>
-                        <th className="p-2 whitespace-nowrap">
                           <div className="font-semibold text-left">Commodity</div>
                         </th>
                         <th className="p-2 whitespace-nowrap">
@@ -61,9 +58,6 @@ const LabHeadAssignment = () => {
                             <div className="flex items-center">
                               <input checked id="checked-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
                             </div>
-                          </td>
-                          <td className="p-2 whitespace-nowrap">
-                            <div className="text-left">{de.commodityGroupText}</div>
                           </td>
                           <td className="p-2 whitespace-nowrap">
                             <div className="text-left">{de.commodityText}</div>
