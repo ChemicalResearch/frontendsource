0a47bdc8e702459a2fc9f36bfa4873475093fdf1
Goutam Das <goutamdas676@gmail.com>
2024-04-28 15:38:38 +0530

    Test Result

--- src/services/verify-test-result.ts
+++ src/services/verify-test-result.ts
@@ -1,31 +1,44 @@
 import { AxiosResponse } from "axios";
 import { apiClient } from "../config/api-client";
 
-export type VerifyTestResult = {
-    id: number;
-    sampleQr: number;
-    chemist: number;
+export type VerifyTestResult = Array<{
+  labHeadName: string;
+  labHeadId: string;
+  qrcode: string;
+  jobNumber: string;
+  certificateurl: string;
+  labDetails: Array<{
+    commodityGroupText:string;
+    commodityGroupId: string;
+    commodityText: string;
+    commodityId: string;
+    parameterText: string;
+    parameterId: string;
+    testmethodText: string;
+    testmethodId: string;
     chemistAssignedOn: string;
-    commodityGrp: number;
-    commodity: number;
-    parameter: number;
-    testMethod: number;
-    result: number;
-    resultUnit: number;
+    chemistActivityStatus: string;
+    verificationStatus: string;
+    verifiedon: string;
+    chemist: string;
+    qa: string;
+    result: string;
+    resultsubmitDate: string;
+    unit: string;
+    chemistAssignedDate: string;
     resultSubmitDate: string;
-    status: number;
-    verifiedBy: number;
-    verificationStatus: number;
-    verifiedOn: string;
-    commodityGrpName: string;
-    commodityName: string;
-    parameterName: string;
-    methodName: string;
-    unitName: string;
-    statusName: string;
-    verificationStatusDsc: string;
-}
+    verifiicationDate: string;
+    assignedChemistId: string;
+    assignedChemistName: string;
+    assignedQAId: string;
+    assignedQAName: string;
+    assignedChemistStatus: string;
+    assignedVerificationStatus: string;
+  }>;
+}>;
 
 export const getVerifyTestResult = (employee_id: number) => {
-    return apiClient.get<any, AxiosResponse<Array<VerifyTestResult>>>(`/assignedtoqa/${employee_id}`)
-}
+  return apiClient.get<any, AxiosResponse<VerifyTestResult>>(
+    `/assignedtoqa/${employee_id}`
+  );
+};
