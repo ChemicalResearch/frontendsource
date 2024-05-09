7a8cbfd74c26e7165937428afaa060ca99c9b1ea
souravspacejoy <126777382+souravspacejoy@users.noreply.github.com>
2024-05-09 21:49:32 +0530

    update sample analysis

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -59,8 +59,8 @@ const SideMenu = () => {
                                 <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
                                 <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
                             </svg>
-                            Assign QR code
-                        </ActiveNavLink>
+                            SAMPLE PREG AND QR ASSIGNMENT
+\                        </ActiveNavLink>
                     </li>
                     {/* <li>
                         <NavLink to="/sample-preparation" >
