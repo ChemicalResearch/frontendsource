948344454d4fcf8b451070eda7b8ad217a03722c
Goutam Das <goutamdas676@gmail.com>
2024-05-19 10:37:21 +0530

    Create JRF Done

--- src/pages/CreateJrf/components/Collection.tsx
+++ src/pages/CreateJrf/components/Collection.tsx
@@ -1,6 +1,8 @@
 import { FC, Fragment, useState } from "react";
 import { Field, Formik, FormikHelpers } from "formik";
-import { getJRFList, GetJRFListResponse } from "../../../services";
+import { getJRFList, GetJRFListResponse, createJRF } from "../../../services";
+import { useMutation } from "@tanstack/react-query";
+import Swal from "sweetalert2";
 
 interface CollectionProps {
   plantModelsByDespatchDate:
@@ -14,6 +16,7 @@ interface InitialValues {
 }
 
 interface CreateJRFInitialValues {
+  plantId: string;
   data: Array<{
     checked: boolean;
     jrfNumber: string;
@@ -22,7 +25,11 @@ interface CreateJRFInitialValues {
 
 const CollectionCard: FC<CollectionProps> = ({ plantModelsByDespatchDate }) => {
   const [data, setData] = useState<GetJRFListResponse>();
-  const [plantId, setPlantId] = useState<string>();
+
+  const mutation = useMutation({
+    mutationFn: createJRF,
+  });
+
   const onSubmit = async (
     values: InitialValues,
     formikHelpers: FormikHelpers<InitialValues>
@@ -30,7 +37,6 @@ const CollectionCard: FC<CollectionProps> = ({ plantModelsByDespatchDate }) => {
     try {
       const { data } = await getJRFList(values);
       setData(data);
-      setPlantId(values.plantId);
     } catch (e) {
       setData([]);
     } finally {
@@ -38,13 +44,21 @@ const CollectionCard: FC<CollectionProps> = ({ plantModelsByDespatchDate }) => {
     }
   };
 
-  const handleSubmitJRF = (
+  const handleSubmitJRF = async (
     values: CreateJRFInitialValues,
     formikHelpers: FormikHelpers<CreateJRFInitialValues>
   ) => {
     try {
-      
-    } catch (e) {
+      const { plantId, data } = values;
+      const sampleIds = data.filter((x) => x.checked).map((x) => x.jrfNumber);
+      const response = await mutation.mutateAsync({ plantId, sampleIds });
+      formikHelpers.resetForm();
+      Swal.fire(
+        "JRF created successfully",
+        "JRF Number is" + response.data.returnJrfNumber
+      );
+    } catch (e:any) {
+      console.log(e.response);
     } finally {
       formikHelpers.setSubmitting(false);
     }
@@ -130,65 +144,68 @@ const CollectionCard: FC<CollectionProps> = ({ plantModelsByDespatchDate }) => {
                   Search
                 </button>
               </div>
+              <Formik
+                initialValues={{
+                  ...createJRFInitialValues,
+                  plantId: values.plantId,
+                }}
+                onSubmit={handleSubmitJRF}
+                enableReinitialize
+              >
+                {(subform) => (
+                  <Fragment>
+                    {data?.length ? (
+                      <div className="mt-10">
+                        <div className="overflow-x-auto">
+                          <table className="table-auto w-full">
+                            <thead className="text-xs font-semibold text-gray-400 bg-gray-50">
+                              <tr>
+                                <th className="p-2 whitespace-nowrap">
+                                  <div className="font-semibold text-left">
+                                    Sample ID
+                                  </div>
+                                </th>
+                              </tr>
+                            </thead>
+                            <tbody className="text-sm divide-y divide-gray-100">
+                              {subform?.values?.data?.map((row, key) => (
+                                <tr key={key}>
+                                  <td className="p-2 whitespace-nowrap">
+                                    <div className="flex items-center">
+                                      <Field
+                                        type="checkbox"
+                                        id={`data.${key}.checked`}
+                                        name={`data.${key}.checked`}
+                                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 mr-2 cursor-pointer"
+                                      />
+                                      <label
+                                        htmlFor={`data.${key}.checked`}
+                                        className="font-medium text-gray-800 cursor-pointer"
+                                      >
+                                        {row.jrfNumber}
+                                      </label>
+                                    </div>
+                                  </td>
+                                </tr>
+                              ))}
+                            </tbody>
+                          </table>
+                        </div>
+                        <div className="flex items-center justify-start mt-5">
+                          <button
+                            onClick={subform?.submitForm}
+                            type="button"
+                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                          >
+                            Submit
+                          </button>
+                        </div>
+                      </div>
+                    ) : null}
+                  </Fragment>
+                )}
+              </Formik>
             </div>
-          )}
-        </Formik>
-        <Formik
-          initialValues={createJRFInitialValues}
-          onSubmit={handleSubmitJRF}
-          enableReinitialize
-        >
-          {({ values, submitForm }) => (
-            <Fragment>
-              {data?.length ? (
-                <div className="mt-10">
-                  <div className="overflow-x-auto">
-                    <table className="table-auto w-full">
-                      <thead className="text-xs font-semibold text-gray-400 bg-gray-50">
-                        <tr>
-                          <th className="p-2 whitespace-nowrap">
-                            <div className="font-semibold text-left">
-                              Sample ID
-                            </div>
-                          </th>
-                        </tr>
-                      </thead>
-                      <tbody className="text-sm divide-y divide-gray-100">
-                        {values?.data?.map((row, key) => (
-                          <tr key={key}>
-                            <td className="p-2 whitespace-nowrap">
-                              <div className="flex items-center">
-                                <Field
-                                  type="checkbox"
-                                  id={`data.${key}.checked`}
-                                  name={`data.${key}.checked`}
-                                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 mr-2 cursor-pointer"
-                                />
-                                <label
-                                  htmlFor={`data.${key}.checked`}
-                                  className="font-medium text-gray-800 cursor-pointer"
-                                >
-                                  {row.jrfNumber}
-                                </label>
-                              </div>
-                            </td>
-                          </tr>
-                        ))}
-                      </tbody>
-                    </table>
-                  </div>
-                  <div className="flex items-center justify-start mt-5">
-                    <button
-                      onClick={submitForm}
-                      type="button"
-                      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                    >
-                      Submit
-                    </button>
-                  </div>
-                </div>
-              ) : null}
-            </Fragment>
           )}
         </Formik>
       </div>
--- src/services/create-jrf.ts
+++ src/services/create-jrf.ts
@@ -24,28 +24,40 @@ type GetJRFListParams = {
   despatchDate: string;
 };
 
-
 export type JRF = {
-    "tcrcRefNumber": string;
-    "tcrcSampleId": string;
-    "commodity": string;
-    "dispatchDate": string;
-    "noOfSample": string;
-    "plantEmail": string;
-    "jrfNumber": string;
-    "jrfUrl": string;
-    "jrfTableModels": string;
-}
+  tcrcRefNumber: string;
+  tcrcSampleId: string;
+  commodity: string;
+  dispatchDate: string;
+  noOfSample: string;
+  plantEmail: string;
+  jrfNumber: string;
+  jrfUrl: string;
+  jrfTableModels: string;
+};
 
 export type GetJRFListResponse = Array<JRF>;
 
-export const getJRFList = (
-    params: GetJRFListParams
-) => {
-  return apiClient.get<any, AxiosResponse<GetJRFListResponse>>(
-    "/getjrflist",
-    {
-      params
-    }
+export const getJRFList = (params: GetJRFListParams) => {
+  return apiClient.get<any, AxiosResponse<GetJRFListResponse>>("/getjrflist", {
+    params,
+  });
+};
+
+export type CreateJRFBody = {
+  plantId: string;
+  sampleIds: Array<string>;
+};
+
+export type CreateJRFResponse = {
+  plantId: string;
+  sampleIds: Array<string>;
+  returnJrfNumber: string;
+};
+
+export const createJRF = (body: CreateJRFBody) => {
+  return apiClient.post<any, AxiosResponse<CreateJRFResponse>>(
+    "/createjrf",
+    body
   );
 };
