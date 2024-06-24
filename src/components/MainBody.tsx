8a28eec2c4137dbb4a5092444cf3ffa1e18ed2ee
Goutam Das <goutamdas676@gmail.com>
2024-06-25 00:31:31 +0530

    iframe

--- src/components/MainBody.tsx
+++ src/components/MainBody.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const MainBody = () => {
     return (
-        <div className="w-full pt-10 px-4 sm:px-6 md:px-8 lg:ps-72">
+        <div className="w-full pt-10 px-4 sm:px-6 md:px-8 lg:ps-72 relative">
             <Outlet />
         </div>
     )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,34 @@
-import { getDashboardData } from "../services";
-import { useQuery } from "@tanstack/react-query";
-import Charts from "../components/Charts";
+// import { getDashboardData } from "../services";
+// import { useQuery } from "@tanstack/react-query";
+// import Charts from "../components/Charts";
 
 function Home() {
-  const { data, isPending } = useQuery({
-    queryKey: ["dashboard"],
-    queryFn: async () => {
-      const { data } = await getDashboardData();
-      return data;
-    },
-  });
+  // const { data, isPending } = useQuery({
+  //   queryKey: ["dashboard"],
+  //   queryFn: async () => {
+  //     const { data } = await getDashboardData();
+  //     return data;
+  //   },
+  // });
 
-  if (isPending) return "Loading...";
+  // if (isPending) return "Loading...";
+  // return (
+  //   <div className="flex flex-wrap gap-10">
+  //     {data
+  //       ? Object.values(data).map((chart, key) => (
+  //           <div key={key} className="flex max-w-[600px] bg-white rounded-xl py-10 px-10">
+  //             <Charts chart={chart} />
+  //           </div>
+  //         ))
+  //       : null}
+  //   </div>
+  // );
   return (
-    <div className="flex flex-wrap gap-10">
-      {data
-        ? Object.values(data).map((chart, key) => (
-            <div key={key} className="flex max-w-[600px] bg-white rounded-xl py-10 px-10">
-              <Charts chart={chart} />
-            </div>
-          ))
-        : null}
-    </div>
+    <iframe
+      className="fixed top-0 left-0 right-0 bottom-0 h-full mt-[90px] ml-[255px] w-iframe"
+      src="https://lookerstudio.google.com/embed/reporting/df3d3d07-699f-4562-830b-b177bf636cc8/page/bk23D"
+      sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"
+    ></iframe>
   );
 }
 
